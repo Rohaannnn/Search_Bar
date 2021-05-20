51ec15238e5a454c53230d72f86b642e4f1bbcd8
Rohaannnn <70089656+Rohaannnn@users.noreply.github.com>
2021-05-20 09:45:10 +0530

    Add files via upload
    1st commit

---
+++ bar.js
@@ -0,0 +1,5 @@
+const icon = document.querySelector('.icon');
+        const Search = document.querySelector('.search');
+        icon.onclick = function(){
+            search.classList.toggle('active')
+        }
